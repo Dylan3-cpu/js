2837a35533235a27b75c9625b20468c04fee1bd7
Dylan Aldiver Rodriguez <194501634+Dylan3-cpu@users.noreply.github.com.>
2025-04-22 10:36:34 -0500

    imprime edad y nomnbre en pantalla

---
+++ js/interpolacion.js
@@ -0,0 +1,4 @@
+const nombre = "Juan";
+const edad = 25;
+const mensaje = `Hola, mi nombre es ${nombre} y tengo ${edad} años.`;
+console.log(mensaje); // Imprime: "Hola, mi nombre es Juan y tengo 25 años." 
