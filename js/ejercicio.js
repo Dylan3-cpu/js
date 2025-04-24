e8f3266a0e83a8eeb5deb219fcc96b5888600fde
Dylan Aldiver Rodriguez <194501634+Dylan3-cpu@users.noreply.github.com.>
2025-04-24 11:03:40 -0500

    peter porker 1.2

--- js/ejercicio.js
+++ js/ejercicio.js
@@ -1,3 +1,3 @@
 let heroes = ["ironman", "capitan america", "black widow"];
-frutos.splice(0, 0, "spiderman"); //se agrega a la araña en en 1, antes de ironman
+heroes.splice(0, 0, "spiderman"); //se agrega a la araña en en 1, antes de ironman
 console.log(heroes); 
