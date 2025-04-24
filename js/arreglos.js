d1bdccc46c8b348261829338c13aefe43e564d36
Dylan Aldiver Rodriguez <194501634+Dylan3-cpu@users.noreply.github.com.>
2025-04-24 10:36:09 -0500

    arreglos

---
+++ js/arreglos.js
@@ -0,0 +1,9 @@
+let frutas = []
+//push agrega al final del arreglo o la lista
+frutas.push("manzana")
+console.log(frutas)
+console.log(frutas.length) //length devuelve la cantidad de elementos que hay en el arreglo o la lista
+//pop se utiliza para eliminar el ultimo elemento del arreglo o la lista
+frutas.pop(); 
+console.log(frutas)
+console.log(frutas.length) 
